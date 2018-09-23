92acf18a408a61a76e2c828c553ce992f3b8f023
DrHaaax <43475057+DrHaaax@users.noreply.github.com>
2018-09-23 21:06:17 +0500

    1

--- game.js
+++ game.js
@@ -47,8 +47,8 @@ function draw() {
     
     for (var i = 1; i < 10; i += 1){
         ctx.fillStyle = 'rgb(' + getRandomColor() + ',' + getRandomColor() + ',' + getRandomColor() + ')';
-        ctx.fillRect(i + canvas.width / freqByteData.length, canvas.height - freqByteData[i] * 1.5, canvas.width / freqByteData.length * 30, canvas.height);
-        ctx.strokeRect(i + canvas.width / freqByteData.length, canvas.height - freqByteData[i] * 1.5, canvas.width / freqByteData.length * 30, canvas.height);
+        ctx.fillRect(i * 30 + canvas.width / freqByteData.length, canvas.height - freqByteData[i] * 1.5, canvas.width / freqByteData.length * 30, canvas.height);
+        ctx.strokeRect(i * 30 + canvas.width / freqByteData.length, canvas.height - freqByteData[i] * 1.5, canvas.width / freqByteData.length * 30, canvas.height);
 		checkFreqHeight(freqByteData[i]);
     }
 }
