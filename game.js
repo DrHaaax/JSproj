e74a016d50fee06c217fe5f0259906f59f9beb93
DrHaaax <43475057+DrHaaax@users.noreply.github.com>
2018-09-24 16:55:16 +0500

    1

--- game.js
+++ game.js
@@ -3,12 +3,12 @@ var analyser, canvas, ctx, random = Math.random, circles = [];
 used = false;
 
 	//initialization of beatDetector
-	var song = new stasilo.BeatDetector({
+	/*var song = new stasilo.BeatDetector({
 		sens: 4, 
 		visualizerFFTSize: 256, 
 		analyserFFTSize: 256, 
 		passFreq: 600,
-		url: 'assets/Devil_Trigger.mp3'}); 
+		url: 'assets/Devil_Trigger.mp3'});*/
 
 window.onload = function() {
     canvas = document.createElement('canvas');
@@ -28,7 +28,7 @@ window.onload = function() {
 
 function setupWebAudio() {
     var audio = document.createElement('audio');
-    audio.src = 'assets/Devil_Trigger.mp3';
+    audio.src = "audio";
     audio.controls = 'true';
     document.body.appendChild(audio);
     audio.style.width = canvas.width + 'px';
@@ -92,7 +92,7 @@ Circle.prototype.draw = function() {
     ctx.restore();
 }
 
-/*function checkFreqHeight(freq) {
+function checkFreqHeight(freq) {
 	if (freq > 80 && used == false) {
 		used = true;
 		var sound = new Audio('assets/sound.mp3');
@@ -104,16 +104,17 @@ Circle.prototype.draw = function() {
 		used = false;
 	}
 	console.log(used);
-}*/
+}
 
-function checkFreqHeight(freq) {
+//for beatDetector
+/*function checkFreqHeight(freq) {
 	if (song.isOnBeat()) {
 		var sound = new Audio('assets/sound.mp3');
 		sound.play();
 		ctx.fillStyle = "red";
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
 	}
-}
+}*/
 
 function avg(allFreq) {
 	var total = 0;
